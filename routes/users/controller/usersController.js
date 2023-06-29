d59e16d8f0ba7527d0c37ab1d0b59caebc6e8b8f
nancyk12 <nancyk12@hotmail.com>
2023-06-29 12:18:41 -0400

    added register route

--- routes/users/controller/usersController.js
+++ routes/users/controller/usersController.js
@@ -1,119 +1,3 @@
-const User = require('../model/User')
-const jwt = require('jsonwebtoken')
-const { createUser, hashPassword, comparePasswords } = require('./usersHelper')
-
-module.exports = {
-    login: async (req, res) => {
-        try {
-            console.log(req.headers);
-            
-            //check if user exists / get the user form the db
-            let foundUser = await User.findOne({username: req.body.username})
-            if (!foundUser) {
-                 throw {
-                    status: 404,
-                    message: "User Not Found"
-                }
-            }          
-
-            // check if password matches
-            let checkedPassword = await comparePasswords(req.body.password, foundUser.password)
-            if (!checkedPassword) {
-                throw {
-                    status: 401,
-                    message: "Invalid Password"
-                }
-            }
-            // console.log(foundUser)
-            let payload = {
-                id: foundUser._id,
-                username: foundUser.username
-            }
-
-            let token = await jwt.sign(payload, process.env.SUPER_SECRET_KEY, {expiresIn: 60*60})
-            
-            res.status(200).json({
-                username: req.body.username,
-                message: "Successful Login!!",
-                token: token
-            })  
-        } catch (error) {
-            res.status(error.status).json(error.message)
-        }
-    },
-    register: async (req, res) => {
-        try {
-            //if foundUser exists throw an error
-            let foundUser = await User.findOne({username: req.body.username})
-            if (foundUser) {
-                throw {
-                    status: 409,
-                    message: "User Exists"
-                }
-            } 
-
-            let newUser = await createUser(req.body)
-            
-            // hash password
-            let hashedPassword = await hashPassword(newUser.password)
-            // console.log(hashedPassword);
-
-            //update newUser object with hashed password
-            newUser.password = hashedPassword
-
-            //saves newUser to DB
-            let savedUser = await newUser.save()
-
-            res.status(200).json({
-                    userObj: savedUser,
-                    message: "Successfully Registered"
-                }) 
-        } catch (error) {
-            res.status(error.status).json(error.message)
-        }
-        
-    },
-    authtoken: async (req, res) => {
-        console.log('!@-------req.decoded-------@!')
-        console.log(req.decoded)
-        
-        let foundUser = await User.findById(req.decoded.id)
-
-        // you can re-issue the token to reset the expiration,
-        // this isn't less secure, it is a design decision that can be less secure
-        //
-        // let payload = {
-        //     id: foundUser._id,
-        //     username: foundUser.username
-        // }
-        // let token = await jwt.sign(payload, process.env.SUPER_SECRET_KEY, {expiresIn: 5*60})
-        // res.status(200).json({
-        //     username: foundUser.username,
-        //     message: "Successful Token Login!!",
-        //     token: token
-        // })
-
-        res.status(200).json({
-            username: foundUser.username,
-            message: "Successful Token Login!!"
-        })
-
-
-    },
-    deleteUser: async (req, res) => {
-        try {
-            let foundUser = await User.findByIdAndDelete(req.decoded.id)
-            // console.log(foundUser)
-            res.send(true)
-        } catch (error) {
-            res.send(false)
-        }
-        
-    }
-    
-
-}
-
 // const login = (req, res) => {
 //     return {
 //         username: req.body.username
@@ -123,3 +7,37 @@ module.exports = {
 // module.exports = {
 //     login
 // }
+
+module.exports = {
+    login: async (req, res) => {
+        try {
+          console.log(req.body);
+            if (req.body.username !== 'Brad' && req.body.username !== 'Janet' ){
+                throw {
+                    status: 404,
+                    message: "User Not Found"
+                }
+            }
+
+            if (req.body.password !== 'abc'){
+                throw {
+                    status: 403
+                }
+            }
+            res.status(200).json({
+                username: req.body.username,
+                password: req.body.password,
+                message: "Successful Login!!"
+            })  
+        } catch (error) {
+            res.status(error.status).json('Error!!')
+        }
+    },
+    register: async (req, res) => {
+       res.status(200).json({
+            message: `Successfully Registered ${req.body.username}`
+        }) 
+    }
+    
+
+}
--- routes/users/users.js
+++ routes/users/users.js
@@ -1,15 +1,22 @@
 var express = require('express');
 var router = express.Router();
-
+var usersController = require('./controller/usersController')
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get('/login', function(req, res) {
+router.get('/login-test', function(req, res) {
+  console.log(req.body)
   res.send({
-    username: 'Janet'
+    username: req.body.username
   })
 })
+
+router.post('/login', usersController.login)
+
+//add route for register
+router.post('/register', usersController.register)
+
 
 module.exports = router;
